2c07ad07256827971b5df48a333b3ddedb6dd41c
Saniya Ahuja <53290877+saniya3@users.noreply.github.com>
2021-01-20 10:43:38 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,58 @@
+const express=require("express");
+const bodyParser=require("body-parser");
+const request=require("request");
+const https=require("https");
+
+const app=express();
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+
+app.get("/",function(req,res){
+   res.sendFile(__dirname+"/signup.html");
+});
+
+app.post("/",function(req,res){
+    var fn=(req.body.fn);
+    var ln=(req.body.ln);
+    var email=(req.body.email);
+
+    var data={
+      members:[
+        {
+          email_address: email ,
+          status: "subscribed",
+          merge_field:{
+              FNAME: fn,
+              LNAME: ln
+          }
+        }
+      ]
+    };
+
+    const jsonData=JSON.stringify(data);
+    const url="https://us7.api.mailchimp.com/3.0/lists/326b7d836d"
+    const options={
+      method:"POST",
+      auth:"saniya:c4fc47cc398ce3fd85735c878cc6af09-us7"
+    }
+    const request=https.request(url,options,function(response){
+      if(response.statusCode===200){
+        res.sendFile(__dirname+"/success.html");
+      } 
+      else{
+        res.sendFile(__dirname+"/failure.html");
+      }
+      response.on("data",function(data){
+         console.log(JSON.parse(data));
+       });
+    });
+  request.write(jsonData);
+  request.end();
+});
+
+app.listen(process.env.PORT || 3000,function(){
+  console.log("server is running");
+})
+
+//c4fc47cc398ce3fd85735c878cc6af09-us7
+//326b7d836d(list)
